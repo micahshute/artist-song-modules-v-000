a52652ce56c47365caa623aa04ded037e00db342
Micah Shute <micah.shute@gmail.com>
2018-07-06 05:31:24 +0000

    automatically backed up by learn

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,28 @@
+module Memorable  
+
+    module ClassMethods
+
+      @@all = []
+
+      def all 
+        @@all
+      end
+
+      def count
+        @@all.length
+      end
+
+      def reset_all
+        @@all.clear
+      end
+  end
+
+  module InstanceMethods
+
+    def initialize
+      self.class.all << self
+    end
+
+  end
+
+end
